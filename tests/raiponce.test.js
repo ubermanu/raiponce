0a328bb431d22d32a812c0d2045a16cc77768711
ubermanu <manu@paca.happy-dev.fr>
2021-05-01 20:09:08 +0200

    Add lerp test Restore scss tests

--- tests/raiponce.test.js
+++ tests/raiponce.test.js
@@ -1,5 +1,11 @@
 import { lerp } from '..'
+import sassTrue from 'sass-true'
 
 it('should support PX for responsive sizes', () => {
-  console.log(lerp({ 768: 10, 1920: 30 }, 10))
+  expect(lerp({ 768: 1, 1920: 3 }, 10)).toEqual(
+    'clamp(1rem, 1.7361111111vw - 0.3333333333rem, 3rem)'
+  )
 })
+
+// Run scss tests
+sassTrue.runSass({ file: 'tests/raiponce.test.scss' }, { describe, it })
