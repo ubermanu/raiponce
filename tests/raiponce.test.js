68d739626918015756deb57f8a8ac1a004c986d6
ubermanu <manu@paca.happy-dev.fr>
2022-08-05 14:01:00 +0200

    add same tests for the JS module

--- tests/raiponce.test.js
+++ tests/raiponce.test.js
@@ -1,10 +1,31 @@
 import { lerp } from '..'
 import * as sassTrue from 'sass-true'
 
-it('should support PX for responsive sizes', () => {
-  expect(lerp({ 768: '10px', 1920: '30px' })).toEqual(
-    'clamp(10px, 1.7361111111vw - 3.3333333333px, 30px)'
-  )
+// Run JS tests
+describe('JS: raiponce.lerp [function]', function () {
+  it('supports PX for responsive sizes', () => {
+    expect(lerp({ 768: '10px', 1920: '30px' })).toEqual(
+      'clamp(10px, 1.7361111111vw - 3.3333333333px, 30px)'
+    )
+  })
+
+  it('supports REM for responsive sizes, with a ratio of 10/1', () => {
+    expect(lerp({ 768: '1rem', 1920: '3rem' }, 10)).toEqual(
+      'clamp(1rem, 1.7361111111vw - 0.3333333333rem, 3rem)'
+    )
+  })
+
+  it('supports REM for responsive sizes, with a ratio of 16/1', () => {
+    expect(lerp({ 360: '1rem', 840: '3.5rem' }, 10)).toEqual(
+      'clamp(1rem, 8.3333333333vw - 0.875rem, 3.5rem)'
+    )
+  })
+
+  it('supports EM for responsive sizes', () => {
+    expect(lerp({ 480: '4em', 800: '7em' }, 10)).toEqual(
+      'clamp(4em, 9.375vw - 0.5em, 7em)'
+    )
+  })
 })
 
 // Run scss tests
